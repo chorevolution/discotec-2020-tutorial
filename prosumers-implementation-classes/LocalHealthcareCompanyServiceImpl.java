4b2812c760d14f14c32dfb80cc381d9769071a2e
Claudio Pompilio <claudio.pompilio@gmail.com>
2020-06-03 18:46:56 +0200

    removed commented code inside the prosumers' implementation classes

--- prosumers-implementation-classes/LocalHealthcareCompanyServiceImpl.java
+++ prosumers-implementation-classes/LocalHealthcareCompanyServiceImpl.java
@@ -39,63 +39,9 @@ public class LocalHealthcareCompanyServiceImpl implements LocalHealthcareCompany
 
     @Override
     public FarmSiteInspectionRequest createFarmSiteInspectionRequest(ChoreographyInstanceMessages choreographyInstanceMessages, String choreographyTaskName, String receiverParticipantName) {
+ 
     	FarmSiteInspectionRequest result = new FarmSiteInspectionRequest();
-    	/**
-		 *	TODO write the code that generates FarmSiteInspectionRequest message 
-		 *	that has to be sent to receiverParticipantName within the interaction belonging to choreographyTaskName
-		 *	You can exploit the following API from choreographyInstanceMessages
-		 *	1. Get a Message using its name 
-		 *		- example: 
-		 *			EcoRoutesRequest message = (EcoRoutesRequest) choreographyInstanceMessages.getMessage("EcoRoutesRequest");
-		 *		A null value is returned if no message has been found.
-		 *	2. Get a list of messages using a message name
-		 *		- example: 
-		 *			List<EcoRoutesRequest> messages = choreographyInstanceMessages.getMessages("EcoRoutesRequest");
-		 *		A null value is returned if no message has been found. 
-		 *	3. Get a message using its name and the name of the sender participant
-		 *		- example: 
-		 *			EcoRoutesRequest message = (EcoRoutesRequest) choreographyInstanceMessages.getMessageSentFromParticipant("EcoRoutesRequest","ND");
-		 *		A null value is returned if no message has been found. 
-		 *	4. Get a list of messages using a message name and a sender participant name
-		 *		- example: 
-		 *			List<EcoRoutesRequest> messages = choreographyInstanceMessages.getMessagesSentFromParticipant("EcoRoutesRequest","ND"); 
-		 *		A null value is returned if no message has been found.
-		 *	5. Get a message using its name, the name of the sender participant and the name of the task in which 
-		 *		the message has been exchanged
-		 *		- example: 
-		 *			EcoRoutesRequest message = (EcoRoutesRequest) choreographyInstanceMessages.getMessageSentFromParticipant("EcoRoutesRequest","ND","Get Eco Routes");	
-		 *		A null value is returned if no message has been found.
-		 *	6. Get a list of messages using a message name, the name of the sender participant and the name of the task in which 
-		 *		the messages has been exchanged
-		 *		- example: 
-		 *			List<EcoRoutesRequest> messages = choreographyInstanceMessages.getMessagesSentFromParticipant("EcoRoutesRequest","ND","Get Eco Routes");
-		 *		A null value is returned if no message has been found.
-		 *	7. Get a message using its name and the name of the receiver participant
-		 *		- example: 
-		 *			RoutesRequest message = (RoutesRequest) choreographyInstanceMessages.getMessageReceivedParticipant("RoutesRequest", "DTS-GOOGLE");
-		 *		A null value is returned if no message has been found. 
-		 *	8. Get a message using its name, the name of the receiver participant and the name of the task in which 
-		 *		the message has been exchanged
-		 *		- example: 
-		 *			RoutesRequest message = (RoutesRequest) choreographyInstanceMessages.getMessageReceivedFromParticipant("RoutesRequest", "DTS-GOOGLE", "Routes Request");;	
-		 *		A null value is returned if no message has been found.
-		 *	9. Get a list of messages using a message name and a receiver participant name
-		 *		- example: 
-         *			List<RoutesRequest> messages = choreographyInstanceMessages.getMessagesReceivedFromParticipant("RoutesRequest", "DTS-GOOGLE");
-		 *	10. Get a list of messages using a message name, the name of the receiver participant and the name of the task in which 
-		 *		the messages has been exchanged
-		 *		- example: 
-		 *			List<RoutesRequest> messages = choreographyInstanceMessages.getMessagesReceivedFromParticipant("RoutesRequest", "DTS-GOOGLE", "Routes Request");
-		 *		A null value is returned if no message has been found.
-		 */	
-//    	FarmBusinessStartupRequest farmBusinessStartupRequest = (FarmBusinessStartupRequest) choreographyInstanceMessages.getMessage("farmBusinessStartupRequest");
-
-//    	FarmBusinessStartupRequest farmBusinessStartupRequest = (FarmBusinessStartupRequest) choreographyInstanceMessages.getMessageSentFromParticipant("farmBusinessStartupRequest","User");
-//    	FarmBusinessStartupRequest farmBusinessStartupRequest = (FarmBusinessStartupRequest) choreographyInstanceMessages.getMessageSentFromParticipant("farmBusinessStartupRequest","User","Farm Business Code Request");
-
-//    	FarmBusinessStartupRequest farmBusinessStartupRequest = (FarmBusinessStartupRequest) choreographyInstanceMessages.getMessageReceivedParticipant("farmBusinessStartupRequest", "Local Healthcare Company");
-    	FarmBusinessStartupRequest farmBusinessStartupRequest = (FarmBusinessStartupRequest) choreographyInstanceMessages.getMessageReceivedFromParticipant("farmBusinessStartupRequest", "Local Healthcare Company", "Farm Business Code Request");
-    	
+    	FarmBusinessStartupRequest farmBusinessStartupRequest = (FarmBusinessStartupRequest) choreographyInstanceMessages.getMessageReceivedFromParticipant("farmBusinessStartupRequest", "Local Healthcare Company", "Farm Business Code Request");    	
     	result.setBreedingMethodologyType(farmBusinessStartupRequest.getFarmBusinessStartupRequestForm().getFarm().getBreedingMethodologyType());
     	result.setCompanyName(farmBusinessStartupRequest.getFarmBusinessStartupRequestForm().getFarm().getCompanyName());
     	result.setFarmName(farmBusinessStartupRequest.getFarmBusinessStartupRequestForm().getFarm().getFarmName());
@@ -110,72 +56,10 @@ public class LocalHealthcareCompanyServiceImpl implements LocalHealthcareCompany
     
     @Override
     public FarmOpeningNotification createFarmOpeningNotification(ChoreographyInstanceMessages choreographyInstanceMessages, String choreographyTaskName, String receiverParticipantName) {
+
     	FarmOpeningNotification result = new FarmOpeningNotification();
-    	/**
-		 *	TODO write the code that generates FarmOpeningNotification message 
-		 *	that has to be sent to receiverParticipantName within the interaction belonging to choreographyTaskName
-		 *	You can exploit the following API from choreographyInstanceMessages
-		 *	1. Get a Message using its name 
-		 *		- example: 
-		 *			EcoRoutesRequest message = (EcoRoutesRequest) choreographyInstanceMessages.getMessage("EcoRoutesRequest");
-		 *		A null value is returned if no message has been found.
-		 *	2. Get a list of messages using a message name
-		 *		- example: 
-		 *			List<EcoRoutesRequest> messages = choreographyInstanceMessages.getMessages("EcoRoutesRequest");
-		 *		A null value is returned if no message has been found. 
-		 *	3. Get a message using its name and the name of the sender participant
-		 *		- example: 
-		 *			EcoRoutesRequest message = (EcoRoutesRequest) choreographyInstanceMessages.getMessageSentFromParticipant("EcoRoutesRequest","ND");
-		 *		A null value is returned if no message has been found. 
-		 *	4. Get a list of messages using a message name and a sender participant name
-		 *		- example: 
-		 *			List<EcoRoutesRequest> messages = choreographyInstanceMessages.getMessagesSentFromParticipant("EcoRoutesRequest","ND"); 
-		 *		A null value is returned if no message has been found.
-		 *	5. Get a message using its name, the name of the sender participant and the name of the task in which 
-		 *		the message has been exchanged
-		 *		- example: 
-		 *			EcoRoutesRequest message = (EcoRoutesRequest) choreographyInstanceMessages.getMessageSentFromParticipant("EcoRoutesRequest","ND","Get Eco Routes");	
-		 *		A null value is returned if no message has been found.
-		 *	6. Get a list of messages using a message name, the name of the sender participant and the name of the task in which 
-		 *		the messages has been exchanged
-		 *		- example: 
-		 *			List<EcoRoutesRequest> messages = choreographyInstanceMessages.getMessagesSentFromParticipant("EcoRoutesRequest","ND","Get Eco Routes");
-		 *		A null value is returned if no message has been found.
-		 *	7. Get a message using its name and the name of the receiver participant
-		 *		- example: 
-		 *			RoutesRequest message = (RoutesRequest) choreographyInstanceMessages.getMessageReceivedParticipant("RoutesRequest", "DTS-GOOGLE");
-		 *		A null value is returned if no message has been found. 
-		 *	8. Get a message using its name, the name of the receiver participant and the name of the task in which 
-		 *		the message has been exchanged
-		 *		- example: 
-		 *			RoutesRequest message = (RoutesRequest) choreographyInstanceMessages.getMessageReceivedFromParticipant("RoutesRequest", "DTS-GOOGLE", "Routes Request");;	
-		 *		A null value is returned if no message has been found.
-		 *	9. Get a list of messages using a message name and a receiver participant name
-		 *		- example: 
-         *			List<RoutesRequest> messages = choreographyInstanceMessages.getMessagesReceivedFromParticipant("RoutesRequest", "DTS-GOOGLE");
-		 *	10. Get a list of messages using a message name, the name of the receiver participant and the name of the task in which 
-		 *		the messages has been exchanged
-		 *		- example: 
-		 *			List<RoutesRequest> messages = choreographyInstanceMessages.getMessagesReceivedFromParticipant("RoutesRequest", "DTS-GOOGLE", "Routes Request");
-		 *		A null value is returned if no message has been found.
-		 */	
-//    	FarmBusinessStartupRequest farmBusinessStartupRequest = (FarmBusinessStartupRequest) choreographyInstanceMessages.getMessage("farmBusinessStartupRequest");
-    	
-//    	FarmBusinessStartupRequest farmBusinessStartupRequest = (FarmBusinessStartupRequest) choreographyInstanceMessages.getMessageSentFromParticipant("farmBusinessStartupRequest","User");
-//    	FarmBusinessStartupRequest farmBusinessStartupRequest = (FarmBusinessStartupRequest) choreographyInstanceMessages.getMessageSentFromParticipant("farmBusinessStartupRequest","User","Farm Business Code Request");   	
-
-//    	FarmBusinessStartupRequest farmBusinessStartupRequest = (FarmBusinessStartupRequest) choreographyInstanceMessages.getMessageReceivedParticipant("farmBusinessStartupRequest", "Local Healthcare Company");
     	FarmBusinessStartupRequest farmBusinessStartupRequest = (FarmBusinessStartupRequest) choreographyInstanceMessages.getMessageReceivedFromParticipant("farmBusinessStartupRequest", "Local Healthcare Company", "Farm Business Code Request");    	
-    	
-    	
-//    	FarmSiteInspectionResponse farmSiteInspectionResponse = (FarmSiteInspectionResponse) choreographyInstanceMessages.getMessage("farmSiteInspectionResponse");
-    	
-//    	FarmSiteInspectionResponse farmSiteInspectionResponse = (FarmSiteInspectionResponse) choreographyInstanceMessages.getMessageSentFromParticipant("farmSiteInspectionResponse","Veterinary");
-//    	FarmSiteInspectionResponse farmSiteInspectionResponse = (FarmSiteInspectionResponse) choreographyInstanceMessages.getMessageSentFromParticipant("farmSiteInspectionResponse","Veterinary","Farm Site Inspection Verbal");
-
-//    	FarmSiteInspectionResponse farmSiteInspectionResponse = (FarmSiteInspectionResponse) choreographyInstanceMessages.getMessageReceivedParticipant("farmSiteInspectionResponse", "Local Healthcare Company");
-    	FarmSiteInspectionResponse farmSiteInspectionResponse = (FarmSiteInspectionResponse) choreographyInstanceMessages.getMessageReceivedFromParticipant("farmSiteInspectionResponse", "Local Healthcare Company", "Farm Site Inspection Verbal");    	
-    		
+    	FarmSiteInspectionResponse farmSiteInspectionResponse = (FarmSiteInspectionResponse) choreographyInstanceMessages.getMessageReceivedFromParticipant("farmSiteInspectionResponse", "Local Healthcare Company", "Farm Site Inspection Verbal");    	    		
     	result.setFarmOpeningRequestForm(farmBusinessStartupRequest.getFarmBusinessStartupRequestForm());
     	result.setSiteInspectionDetails(farmSiteInspectionResponse.getSiteInspectionVerbal());
     	return result;
@@ -183,63 +67,9 @@ public class LocalHealthcareCompanyServiceImpl implements LocalHealthcareCompany
     
     @Override
     public FarmBusinessStartupResponse createFarmBusinessStartupResponse(ChoreographyInstanceMessages choreographyInstanceMessages, String choreographyTaskName, String receiverParticipantName) {
-    	FarmBusinessStartupResponse result = new FarmBusinessStartupResponse();
-    	/**
-		 *	TODO write the code that generates FarmBusinessStartupResponse message 
-		 *	that has to be sent to receiverParticipantName within the interaction belonging to choreographyTaskName
-		 *	You can exploit the following API from choreographyInstanceMessages
-		 *	1. Get a Message using its name 
-		 *		- example: 
-		 *			EcoRoutesRequest message = (EcoRoutesRequest) choreographyInstanceMessages.getMessage("EcoRoutesRequest");
-		 *		A null value is returned if no message has been found.
-		 *	2. Get a list of messages using a message name
-		 *		- example: 
-		 *			List<EcoRoutesRequest> messages = choreographyInstanceMessages.getMessages("EcoRoutesRequest");
-		 *		A null value is returned if no message has been found. 
-		 *	3. Get a message using its name and the name of the sender participant
-		 *		- example: 
-		 *			EcoRoutesRequest message = (EcoRoutesRequest) choreographyInstanceMessages.getMessageSentFromParticipant("EcoRoutesRequest","ND");
-		 *		A null value is returned if no message has been found. 
-		 *	4. Get a list of messages using a message name and a sender participant name
-		 *		- example: 
-		 *			List<EcoRoutesRequest> messages = choreographyInstanceMessages.getMessagesSentFromParticipant("EcoRoutesRequest","ND"); 
-		 *		A null value is returned if no message has been found.
-		 *	5. Get a message using its name, the name of the sender participant and the name of the task in which 
-		 *		the message has been exchanged
-		 *		- example: 
-		 *			EcoRoutesRequest message = (EcoRoutesRequest) choreographyInstanceMessages.getMessageSentFromParticipant("EcoRoutesRequest","ND","Get Eco Routes");	
-		 *		A null value is returned if no message has been found.
-		 *	6. Get a list of messages using a message name, the name of the sender participant and the name of the task in which 
-		 *		the messages has been exchanged
-		 *		- example: 
-		 *			List<EcoRoutesRequest> messages = choreographyInstanceMessages.getMessagesSentFromParticipant("EcoRoutesRequest","ND","Get Eco Routes");
-		 *		A null value is returned if no message has been found.
-		 *	7. Get a message using its name and the name of the receiver participant
-		 *		- example: 
-		 *			RoutesRequest message = (RoutesRequest) choreographyInstanceMessages.getMessageReceivedParticipant("RoutesRequest", "DTS-GOOGLE");
-		 *		A null value is returned if no message has been found. 
-		 *	8. Get a message using its name, the name of the receiver participant and the name of the task in which 
-		 *		the message has been exchanged
-		 *		- example: 
-		 *			RoutesRequest message = (RoutesRequest) choreographyInstanceMessages.getMessageReceivedFromParticipant("RoutesRequest", "DTS-GOOGLE", "Routes Request");;	
-		 *		A null value is returned if no message has been found.
-		 *	9. Get a list of messages using a message name and a receiver participant name
-		 *		- example: 
-         *			List<RoutesRequest> messages = choreographyInstanceMessages.getMessagesReceivedFromParticipant("RoutesRequest", "DTS-GOOGLE");
-		 *	10. Get a list of messages using a message name, the name of the receiver participant and the name of the task in which 
-		 *		the messages has been exchanged
-		 *		- example: 
-		 *			List<RoutesRequest> messages = choreographyInstanceMessages.getMessagesReceivedFromParticipant("RoutesRequest", "DTS-GOOGLE", "Routes Request");
-		 *		A null value is returned if no message has been found.
-		 */	
-//    	FarmRegistrationNotification farmRegistrationNotification = (FarmRegistrationNotification) choreographyInstanceMessages.getMessage("farmRegistrationNotification");
-    	
-//    	FarmRegistrationNotification farmRegistrationNotification = (FarmRegistrationNotification) choreographyInstanceMessages.getMessageSentFromParticipant("farmRegistrationNotification","Registry Office");
-//    	FarmRegistrationNotification farmRegistrationNotification = (FarmRegistrationNotification) choreographyInstanceMessages.getMessageSentFromParticipant("farmRegistrationNotification","Registry Office","Farm Business Startup Notification");    	
 
-//    	FarmRegistrationNotification farmRegistrationNotification = (FarmRegistrationNotification) choreographyInstanceMessages.getMessageReceivedParticipant("farmRegistrationNotification", "Local Healthcare Company");
-    	FarmRegistrationNotification farmRegistrationNotification = (FarmRegistrationNotification) choreographyInstanceMessages.getMessageReceivedFromParticipant("farmRegistrationNotification", "Local Healthcare Company", "Farm Business Startup Notification");     	
-    	
+    	FarmBusinessStartupResponse result = new FarmBusinessStartupResponse();
+    	FarmRegistrationNotification farmRegistrationNotification = (FarmRegistrationNotification) choreographyInstanceMessages.getMessageReceivedFromParticipant("farmRegistrationNotification", "Local Healthcare Company", "Farm Business Startup Notification");     	    	
     	result.setFarmCode(farmRegistrationNotification.getFarmCode());
     	return result;
     }
